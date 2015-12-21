3047e1924b3dffd9b581c1671622a1495047b183
Storken <reinholdsson.andreas@gmail.com>
2015-12-21 17:42:04 +0100

    Fixed naming

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once("classes/DB.class.php");
+require_once("Classes/DB.class.php");
 
 # $url_params blir en array med alla "värden" som står efter ? avgränsade med /
 # ex. /Posts/single/11 kommer ge en array med 3 värden som är Posts, single och 11
@@ -8,7 +8,7 @@ $url_parts = getUrlParts($_GET);
 $class  = array_shift($url_parts); # tar ut första värdet och lägger den i $class 
 $method = array_shift($url_parts); # tar ut andra värdet och lägger den i $method
 
-require_once("classes/".$class.".class.php"); # Hämta in klassfilen för den klass vi ska anropa
+require_once("Classes/".$class.".class.php"); # Hämta in klassfilen för den klass vi ska anropa
 $data = $class::$method($url_parts); # Anropa metoden vill vill köra på klassen vi har fårr från vår URL samt skicka med övriga parametrar in till den metoden
 
 // listen if we got a redirect request and load that location emidiatly
