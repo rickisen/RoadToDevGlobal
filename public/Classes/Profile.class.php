80122070a411299a46adbf4287b4d582ff345259
rickisen <rickard.spam@gmail.com>
2015-12-23 15:05:24 +0100

    added a profile clas

--- public/Classes/Profile.class.php
+++ public/Classes/Profile.class.php
@@ -1,11 +1,10 @@
 <?php
 
 class Profile{
-	private static $instance;
 	private function __construct(){}
 	private function __clone(){}
 
-	function displayUser($id){
+	static function displayUser($id){
 		return ['loadview' => 'profile', 'user' => new User($id) ];
 	}
 }
