f4ee7a93b54b58f4566f7525b8a84370e1635210
carlowski <carl@carlgranstrom.se>
2015-12-30 19:59:19 +0100

    Created logged in user nav + small template fixes

--- public/Scripts/jquery.js
+++ public/Scripts/jquery.js
@@ -1,56 +1,61 @@
 /* DROPDOWNS */
 $(document).ready(function(){
 
+  // OPEN USER NAV MENU
+  $(".menu-opener").click(function(){
+  $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+  });
 
-$(".dp1 dt a").on('click', function() {
-$(".dp1 dd ul").slideToggle('fast');
-});
+  // LANDING PAGE FILTER DROPDOWNS
+  $(".dp1 dt a").on('click', function() {
+  $(".dp1 dd ul").slideToggle('fast');
+  });
 
-$(".dp1 dd ul li a").on('click', function() {
-$(".d1 dd ul").hide();
-});
+  $(".dp1 dd ul li a").on('click', function() {
+  $(".d1 dd ul").hide();
+  });
 
-$(".dp2 dt a").on('click', function() {
-$(".dp2 dd ul").slideToggle('fast');
-});
+  $(".dp2 dt a").on('click', function() {
+  $(".dp2 dd ul").slideToggle('fast');
+  });
 
-$(".dp2 dd ul li a").on('click', function() {
-$(".d2 dd ul").hide();
-});
+  $(".dp2 dd ul li a").on('click', function() {
+  $(".d2 dd ul").hide();
+  });
 
-$(".dp3 dt a").on('click', function() {
-$(".dp3 dd ul").slideToggle('fast');
-});
+  $(".dp3 dt a").on('click', function() {
+  $(".dp3 dd ul").slideToggle('fast');
+  });
 
-$(".dp3 dd ul li a").on('click', function() {
-$(".d3 dd ul").hide();
-});
+  $(".dp3 dd ul li a").on('click', function() {
+  $(".d3 dd ul").hide();
+  });
 
-function getSelectedValue(id) {
-return $("#" + id).find("dt a span.value").html();
-}
+  function getSelectedValue(id) {
+  return $("#" + id).find("dt a span.value").html();
+  }
 
-$(document).bind('click', function(e) {
-var $clicked = $(e.target);
-if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
-});
+  $(document).bind('click', function(e) {
+  var $clicked = $(e.target);
+  if (!$clicked.parents().hasClass("dropdown")) $(".dropdown dd ul").hide();
+  });
 
-$('.mutliSelect input[type="checkbox"]').on('click', function() {
+  $('.mutliSelect input[type="checkbox"]').on('click', function() {
 
-var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
-  title = $(this).val() + ",";
+  var title = $(this).closest('.mutliSelect').find('input[type="checkbox"]').val(),
+    title = $(this).val() + ",";
 
-if ($(this).is(':checked')) {
-  var html = '<span title="' + title + '">' + title + '</span>';
-  $('.multiSel').append(html);
-  $(".hida").hide();
-} else {
-  $('span[title="' + title + '"]').remove();
-  var ret = $(".hida");
-  $('.dp1 dt a').append(ret);
+  if ($(this).is(':checked')) {
+    var html = '<span title="' + title + '">' + title + '</span>';
+    $('.multiSel').append(html);
+    $(".hida").hide();
+  } else {
+    $('span[title="' + title + '"]').remove();
+    var ret = $(".hida");
+    $('.dp1 dt a').append(ret);
 
-}
-});
+  }
+  });
 
 
 });
