5c05e3230709f446ff489437c425bd6a493c2f21
carlowski <carl@carlgranstrom.se>
2016-01-05 12:15:36 +0100

    Added lobby leader prompt and changed index.twig structure

--- public/Scripts/jquery.js
+++ public/Scripts/jquery.js
@@ -57,5 +57,29 @@ $(document).ready(function(){
   }
   });
 
+  // Lobby prompt
+  $(function(){
+  $(document).keypress(function(e) {
+    cwrite(e.which,'Keypress event');
+    if(e.which==120) custom_dialog_toggle('Keypress x','You opened this window by pressing x');
+    });
+  });
+
+function custom_dialog_toggle(title,text,buttons) {
+  if(typeof title !=='undefined') $('#dlg-header').html(title);
+  if(typeof text !=='undefined') $('#dlg-content').html(text);
+  cwrite('Current state: '+$('#dialog_state').prop("checked"),'custom_dialog_toggle');
+  $('#dialog_state').prop("checked", !$('#dialog_state').prop("checked"));
+}
+
+function cwrite(str,title) {
+  var ce = $('#console');
+  var sg = "<p>";
+  if(typeof title !=='undefined') sg = sg+"<em>"+title+"</em> ";
+  sg = sg+str+"</p>";
+  ce.prepend(sg);
+  //if(ce.children("p").length>6) ce.children("p").first().remove();
+}
+
 
 });
