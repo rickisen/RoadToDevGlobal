5bfeba2cf0a644182e8bf705324d4cc0e8b234c7
carlowski <carl@carlgranstrom.se>
2016-01-06 13:44:09 +0100

    Set up loading to lobby scenario for testing, leader prompt now pops up on load

---
+++ public/Classes/LobbyLoader.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class LobbyLoader{
+	private function __construct(){}
+	private function __clone(){}
+
+	static function searchLobby(){
+		return ['loadview' => 'searchingformatch' ];
+	}
+}
