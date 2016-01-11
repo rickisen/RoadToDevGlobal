a652a76c41d958b1ee26982818a5cb97b6d23546
Storken <reinholdsson.andreas@gmail.com>
2016-01-11 19:41:24 +0100

    Added tip-of-the-day to the loadingpage and also made that when you press play in landingpage when not logged in you will come to a construction page

--- public/Classes/LobbyLoader.class.php
+++ public/Classes/LobbyLoader.class.php
@@ -43,7 +43,27 @@ class LobbyLoader {
 					$_SESSION['currentUser']->setLobby($lobbyId);
           return Lobby::viewLobby();
       } else {
-          return ['loadview' => 'loadingpage' ];
+          return ['loadview' => 'loadingpage', 'randomTip' => self::getRandomTip()];
       }
+    }
+
+    static function getRandomTip(){
+
+      $tips = array(
+        'If you have any questions you might find your answers in the FAQ-tab.',
+        'If someone is acting rude or griefs you can report them.',
+        'It\'s always better to be nice to other players in the long run.',
+        'Respect the elders',
+        'Life is real until you rip',
+        'The red bulls are for drinking',
+        'They don\'t deserve for rekt',
+        'As a premium member you will have access to more features on the site.',
+        'One player in the lobby will get the leader role and it\'s up to the leader to make sure that you start up a game together.',
+        'As soon as you land in a lobby you can join a team chat together with your lobby members',
+        'You will know who the lobby leader is by looking at the profile portraits in the lobby.'
+        );
+      $random = mt_rand(0, count($tips)-1);
+
+      return $tips[$random];
     }
 }
---
+++ public/Classes/UnderConstruction.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class UnderConstruction{
+	private function __construct(){}
+	private function __clone(){}
+
+	static function viewConstruction(){
+		return ['loadview' => 'underconstruction' ];
+	}
+}
