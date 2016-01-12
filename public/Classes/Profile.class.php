e215b09e342a1b55e242bf0a3352c008ded95e8e
rickisen <rickard.spam@gmail.com>
2016-01-12 17:32:29 +0100

    added support for new teams

--- public/Classes/Profile.class.php
+++ public/Classes/Profile.class.php
@@ -5,22 +5,12 @@ class Profile{
 	private function __clone(){}
 
 	static function displayUser($id){
-		$user = new User($id[0]);
-		$user->fetchSteamStats();
 		$database = DB::getInstance();
 
-		$qGetTeamFromId = '
-		SELECT team.id
-		FROM team
-				WHERE team.id = '.$user->inTeam.'
-				LIMIT 1
-		';
-
-		$result = $database->query($qGetTeamFromId);
-
-		if( $result->num_rows > 0 ){
-			$row = $result->fetch_assoc();
-			$team = new Team($row['id']); }
+		$user = new User($id[0]);
+		if ($user->inTeam > 0 ){  
+			$team = new Team($user->inTeam);
+		}
 
 		return ['loadview' => 'playerprofile', 'user' => $user, 'team' => $team ];
 	}
