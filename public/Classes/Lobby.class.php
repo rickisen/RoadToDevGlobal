11bbceb38199bec5cd6dc22f10ab47d3859887d2
Tobias <tob.jan87@gmail.com>
2016-01-14 17:29:45 +0100

    made changes in lobby.class and added some structure in twig templates and added comments for later adjustments in css

--- public/Classes/Lobby.class.php
+++ public/Classes/Lobby.class.php
@@ -5,33 +5,35 @@ class Lobby {
 	private function __clone(){}
 
 	static function viewLobby(){
-      $database = DB::getInstance();
+    $database = DB::getInstance();
 
-      // if someone tries to hackinto another
-      // lobby send them back to the landingpage
-      if ($_SESSION['currentUser']->inLobby == 'nolobby') {
-		return ['loadview' => 'landingpage' ];
-      }
-
-      $lobbyId    = $_SESSION['currentUser']->inLobby;
-      $mySteamID  = $_SESSION['currentUser']->steamId;
-      $lobbyMates = array();
-
-      $qGetLobbyMates = '
-        SELECT steam_id
-        FROM lobby 
-        WHERE lobby_id  = "'.$lobbyId.'"
-          AND steam_id != "'.$mySteamID.'"
-      ';
-
-      $result = $database->query($qGetLobbyMates);
-      if( $result->num_rows > 0){
-          while ($row = $result->fetch_assoc()) {
-              $lobbyMates[] = new User($row['steam_id']);
-          }
-      } else {
-          echo "Failed to get users from Lobby: $lobbyId : ".$database->error;
-      }
-      return ['loadview' => 'lobby', 'lobbyMates' => $lobbyMates, 'lobbyId' => $lobbyId];
+    // if someone tries to hackinto another
+    // lobby send them back to the landingpage
+    if ($_SESSION['currentUser']->inLobby == 'nolobby') {
+	   return ['loadview' => 'landingpage' ];
     }
+
+    $lobbyId    = $_SESSION['currentUser']->inLobby;
+    $mySteamID  = $_SESSION['currentUser']->steamId;
+    $lobbyMates = array();
+
+    $qGetLobbyMates = '
+      SELECT *
+      FROM lobby 
+      WHERE lobby_id  = "'.$lobbyId.'"
+    ';
+
+    $result = $database->query($qGetLobbyMates);
+    if( $result->num_rows > 0){
+      while ($row = $result->fetch_assoc()) {
+        if($row['is_leader'] == 1 ){
+          $leader = new User($row['steam_id']);
+        }        
+        $lobbyMates[] = new User($row['steam_id']);
+      }
+    } else {
+        echo "Failed to get users from Lobby: $lobbyId : ".$database->error;
+    }
+    return ['loadview' => 'lobby', 'lobbyMates' => $lobbyMates, 'lobbyId' => $lobbyId, 'leader' => $leader];
+  }
 }
