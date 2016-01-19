7859fc64fedf7bd9acb589d6dcb3b135fe5a5ff7
Andreas Reinholdsson <Andreas Reinholdsson>
2016-01-19 15:23:22 +0100

    Added a confirm pop-up to the remove button

--- public/Scripts/jquery.js
+++ public/Scripts/jquery.js
@@ -126,7 +126,6 @@ $(document).ready(function(){
 
   // Lobby and EditTeam prompt
 
-
   $(function(){
   $(document).keypress(function(e) {
     cwrite(e.which,'Keypress event');
@@ -134,20 +133,20 @@ $(document).ready(function(){
     });
   });
 
-function custom_dialog_toggle(title,text,buttons) {
-  if(typeof title !=='undefined') $('#dlg-header').html(title);
-  if(typeof text !=='undefined') $('#dlg-content').html(text);
-  cwrite('Current state: '+$('#dialog_state').prop("checked"),'custom_dialog_toggle');
-  $('#dialog_state').prop("checked", !$('#dialog_state').prop("checked"));
-}
+  function custom_dialog_toggle(title,text,buttons) {
+    if(typeof title !=='undefined') $('#dlg-header').html(title);
+    if(typeof text !=='undefined') $('#dlg-content').html(text);
+    cwrite('Current state: '+$('#dialog_state').prop("checked"),'custom_dialog_toggle');
+    $('#dialog_state').prop("checked", !$('#dialog_state').prop("checked"));
+  }
 
-function cwrite(str,title) {
-  var ce = $('#console');
-  var sg = "<p>";
-  if(typeof title !=='undefined') sg = sg+"<em>"+title+"</em> ";
-  sg = sg+str+"</p>";
-  ce.prepend(sg);
-  //if(ce.children("p").length>6) ce.children("p").first().remove();
-}
+  function cwrite(str,title) {
+    var ce = $('#console');
+    var sg = "<p>";
+    if(typeof title !=='undefined') sg = sg+"<em>"+title+"</em> ";
+    sg = sg+str+"</p>";
+    ce.prepend(sg);
+    //if(ce.children("p").length>6) ce.children("p").first().remove();
+  }
 
-});
+  });
