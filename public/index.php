338df43df03aad6f7a1d8f0810dd2b5d6d84cdbb
rickisen <rickard.spam@gmail.com>
2016-01-25 13:57:10 +0100

    removed swedish comments

--- public/index.php
+++ public/index.php
@@ -18,15 +18,13 @@ if( ! empty($_POST) && isset($_POST['postHandler']) ) {
 // Unless we didn't get any get request
 if( ! empty($_GET)  ) {
 
-  # $url_params blir en array med alla "värden" som står efter ? avgränsade med /
-  # ex. /Posts/single/11 kommer ge en array med 3 värden som är Posts, single och 11
   $url_parts = getUrlParts($_GET) ;
 
-  $class  = array_shift($url_parts); # tar ut första värdet och lägger den i $class
-  $method = array_shift($url_parts); # tar ut andra värdet och lägger den i $method
+  $class  = array_shift($url_parts); 
+  $method = array_shift($url_parts); 
 
-  require_once("Classes/views/".$class.".class.php"); # Hämta in klassfilen för den klass vi ska anropa
-  $data = $class::$method($url_parts); # Anropa metoden vill vill köra på klassen vi har fårr från vår URL samt skicka med övriga parametrar in till den metoden
+  require_once("Classes/views/".$class.".class.php"); 
+  $data = $class::$method($url_parts); 
 } else {
   $data = array('loadview' => 'landingpage');
 }
